57d6d95447c330efdae7cbae300f27cabaa4a5e5
djp <djp@djps-MacBook-Pro-2.local>
2020-02-12 13:29:25 +0800

    working on subset.v

--- src/Subset.v
+++ src/Subset.v
@@ -71,5 +71,142 @@ Eval compute in pred_strong2 (exist _ 2 two_gt0).
 Extraction pred_strong2.
 
 
+Definition pred_strong3 (s : {n : nat | n > 0}) : {m : nat | proj1_sig s = S m} :=
+  match s return {m : nat | proj1_sig s = S m} with
+  | exist 0 pf => match zgtz pf with end
+  | exist (S n') _ => exist _ n' (eq_refl _)
+  end.
 
-                       
+Eval compute in pred_strong3 (exist _ 2 two_gt0).
+
+Extraction pred_strong3.
+
+Definition pred_strong4 : forall n : nat, n > 0 -> {m : nat | n = S m}.
+  refine (fun n =>
+            match n with
+            | 0 => fun _ => False_rec _ _
+            | S n' => fun _ => exist _ n' _
+            end).
+  -
+    inversion g.
+  -
+    reflexivity.
+Defined.
+
+Extraction pred_strong4.
+
+Definition pred_strong4' : forall n : nat, n > 0 -> {m : nat | n = S m}.
+  refine (fun n =>
+            match n with
+            | 0 => fun _ => False_rec _ _
+            | S n' => fun _ => exist _ n' _
+            end); abstract crush.
+Defined.
+
+Print pred_strong4'.
+
+Eval compute in pred_strong4 (two_gt0).
+
+Print sumbool.
+
+Definition eq_nat_dec : forall n m : nat, sumbool (n = m) (n <> m).
+  refine (fix F (n m : nat) : sumbool (n = m) (n <> m) :=
+            match n, m with
+            | 0, 0 => left _ _
+            | S n', S m' => if F n' m' then left _ _ else right _ _
+            | _, _ => right _ _
+            end).
+  -
+    reflexivity.
+  -
+    unfold not. intro H; inversion H.
+  -
+    unfold not; intro H; inversion H.
+  -
+    rewrite e; reflexivity.
+  -
+    unfold not; intro H; inversion H; apply n0; assumption.
+Defined.
+
+Eval compute in eq_nat_dec 2 2.
+
+Eval compute in eq_nat_dec 2 3.
+
+Extraction eq_nat_dec.
+
+Section In_dec.
+  Variable A : Set.
+  Variable A_eq_dec : forall x y : A, {x = y} + {x <> y}.
+
+  Definition In_dec : forall (x : A) (ls : list A), {In x ls} + {~ In x ls}.
+    refine (fix f (x : A) (ls : list A) :=
+              match ls with
+              | nil => right _ _
+              | y :: ls' =>
+                if A_eq_dec x y
+                then
+                  left _ _
+                else
+                  if f x ls'
+                  then left _ _
+                  else right _ _
+              end                
+           ).
+    -
+      intros H. unfold In in H. assumption.
+    -
+      unfold In. left. rewrite e. reflexivity.
+    -
+      simpl. right. assumption.
+    -
+      simpl. intro H. destruct H.
+      apply n; symmetry; assumption.
+      apply n0; assumption.
+  Defined.
+End In_dec.
+
+Eval compute in In_dec eq_nat_dec 2 (1 :: 2 :: nil).
+
+Eval compute in In_dec eq_nat_dec 3 (1 :: 2 :: nil).
+
+Extraction In_dec.
+
+Inductive maybe (A : Set) (P : A -> Prop) : Set :=
+| Unknown : maybe P
+| Found : forall x : A, P x -> maybe P.
+
+Definition pred_strong7 : forall n : nat, maybe (fun m : nat => n = S m).
+  refine (fun (n : nat) =>
+            match n return maybe (fun m : nat => n = S m) with
+            | 0 => Unknown _
+            | S n' => Found _ _ _
+            end              
+         ).
+  trivial.
+Qed.
+
+Eval compute in pred_strong7 2.
+
+Eval compute in pred_strong7 0.
+  
+Print sumor.
+
+Extraction pred_strong7.
+
+Definition pred_strong8 : forall n : nat, sumor (sig (fun m : nat => n = S m)) (n = 0).
+  refine (fun n : nat =>
+            match n return sumor (sig (fun m : nat => n = S m)) (n = 0) with
+            | 0 => inright _ _
+            | S n' => inleft _ (exist _ n' _)
+            end
+         ); repeat reflexivity.
+Qed.
+
+Eval compute in pred_strong8 2.
+
+Eval compute in pred_strong8 0.
+
+
+Notation {{x | P}} := maybe (fun x => P).
+Notation "??" := (Unknown _).
+Notation "[|x|]" := (Found _ x _).
